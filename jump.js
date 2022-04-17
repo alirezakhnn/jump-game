fb345cfdb085d96286f9a8ebc668c6933c5b74fc
alireza <alirezakhanmohammad>
2022-04-17 23:54:35 +0430

    function of character added

--- jump.js
+++ jump.js
@@ -1,3 +1,11 @@
 let full = document.getElementById('full');
 let character = document.querySelector('.character');
 let block = document.querySelector('.block');
+
+
+function jump() {
+    character.classList.add('animation');
+    setTimeout(() => {
+        character.classList.remove('animation');
+    }, 300)
+}
