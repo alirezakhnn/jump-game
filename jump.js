c5a6776c65ffd876d185e825b9096c9ba81d8e85
alireza <alirezakhanmohammad>
2022-04-18 14:22:27 +0430

    computed styles and starta button works almost

--- jump.js
+++ jump.js
@@ -3,6 +3,8 @@ var character = document.querySelector('.character');
 var block = document.querySelector('.block');
 var score = document.querySelector('.score');
 var counter = 0;
+var characterTop = window.getComputedStyle(character).getPropertyValue('top');
+var blockLeft = window.getComputedStyle(block).getPropertyValue('left');
 var btnAgain = document.querySelector('.restart-again-btn');
 var startBtn = document.querySelector('.restart button:nth-child(1)');
 var health = document.querySelector('.health');
@@ -14,4 +16,9 @@ function jump() {
         character.classList.remove('animation');
     }, 500);
 }
-//need new plan
+startBtn.addEventListener('click', function() {
+    setInterval(() => {
+        block.classList.add('blockanim');
+    }, 1500)
+
+})
