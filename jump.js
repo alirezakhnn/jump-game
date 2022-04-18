d0707b66d4c74ee33c216e582f0b2ddb72ef9084
alireza <alirezakhanmohammad>
2022-04-18 13:27:46 +0430

    variables and buttons added

--- jump.js
+++ jump.js
@@ -1,11 +1,17 @@
-let full = document.getElementById('full');
-let character = document.querySelector('.character');
-let block = document.querySelector('.block');
-
+var full = document.getElementById('full');
+var character = document.querySelector('.character');
+var block = document.querySelector('.block');
+var score = document.querySelector('.score');
+var counter = 0;
+var btnAgain = document.querySelector('.restart-again-btn');
+var startBtn = document.querySelector('.restart button:nth-child(1)');
+var health = document.querySelector('.health');
+var remain = 5;
 
 function jump() {
     character.classList.add('animation');
     setTimeout(() => {
         character.classList.remove('animation');
-    }, 300)
+    }, 500);
 }
+//need new plan
