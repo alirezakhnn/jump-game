5184cd5c40d18688aded9c704f1e6a9cd8fa3c56
alireza <alirezakhanmohammad>
2022-04-21 13:36:11 +0430

    button fixed and height of block decreased and time of character jump increased

--- jump.js
+++ jump.js
@@ -3,8 +3,8 @@ var character = document.querySelector('.character');
 var block = document.querySelector('.block');
 var score = document.querySelector('.score');
 var counter = 0;
-var charTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+let charTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 var btnAgain = document.querySelector('.restart button:nth-child(2)');
 var startBtn = document.querySelector('.restart button:nth-child(1)');
 var health = document.querySelector('.health');
@@ -14,18 +14,18 @@ function jump() {
     character.classList.add('animation');
     setTimeout(() => {
         character.classList.remove('animation');
-    }, 500);
+    }, 600);
 }
-startBtn.addEventListener('click', function() {
-    block.classList.add('blockanim');
+startBtn.addEventListener('click', () => {
+    block.classList.add('block-anim');
     startBtn.innerHTML = '2x to stop';
     health.innerHTML = 'you have ' + remain + ' health';
     score.innerHTML = 'you have ' + counter + ' points';
-    startBtn.ondblclick = () => {
-        block.classList.remove('blockanim');
-        startBtn.innerHTML = 'start';
-    }
 })
+startBtn.ondblclick = () => {
+    block.classList.remove('block-anim');
+    startBtn.innerHTML = 'start';
+}
 
 btnAgain.onclick = () => {
     window.location.reload();
