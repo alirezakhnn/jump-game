7794996d7a8a8d05cf6411da21e03b1798c82ae4
alireza <alirezakhanmohammad>
2022-04-26 12:29:10 +0430

    completed

--- jump.js
+++ jump.js
@@ -3,8 +3,7 @@ var character = document.querySelector('.character');
 var block = document.querySelector('.block');
 var score = document.querySelector('.score');
 var counter = 0;
-let charTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+var guide = document.querySelector('.guide');
 var btnAgain = document.querySelector('.restart button:nth-child(2)');
 var startBtn = document.querySelector('.restart button:nth-child(1)');
 var health = document.querySelector('.health');
@@ -21,7 +20,38 @@ startBtn.addEventListener('click', () => {
     startBtn.innerHTML = '2x to stop';
     health.innerHTML = 'you have ' + remain + ' health';
     score.innerHTML = 'you have ' + counter + ' points';
+    setInterval(() => {
+        let charTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        //lose condition =>
+        if (blockLeft <= 26 && blockLeft >= 0 && charTop >= 240) {
+            startBtn.innerHTML = 'continue';
+            alert('crashed !');
+            score.innerHTML = 'you have ' + counter + ' points';
+            block.classList.remove('block-anim');
+            remain--;
+            health.innerHTML = 'you have ' + remain + ' health';
+            if (startBtn.onclick) {
+                block.classList.add('block-anim');
+                startBtn.innerHTML = '2x to stop';
+            }
+        } else if (startBtn.innerHTML == '2x to stop') { //win condition =>
+            counter++;
+            score.innerHTML = 'you have ' + counter + ' points';
+        } else {
+            score.innerHTML = 'you have ' + counter + ' points';
+        }
+        if (remain == 0) { //finish condition =>
+            startBtn.disabled = true;
+            startBtn.innerHTML = 'disabled';
+            guide.innerHTML = 'game is finished';
+            guide.style.color = 'orange';
+        }
+    }, 124)
+
+    //my problem is handle the score;
 })
+
 startBtn.ondblclick = () => {
     block.classList.remove('block-anim');
     startBtn.innerHTML = 'start';
